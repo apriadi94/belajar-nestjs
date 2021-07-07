234e4630525fbd2a27f50c48b75b406fed9311cd
Apriadi <dev.apriadi@gmail.com>
2021-07-07 19:54:45 +0700

    membuat route untuk user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.model';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectModel('Users') private readonly userModel: Model<UserDocument>
+    ){}
+    async getData() {
+        return this.userModel.find().exec()
+    }
+}
